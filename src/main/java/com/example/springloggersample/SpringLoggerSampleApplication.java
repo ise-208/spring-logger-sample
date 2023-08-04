15594fc3627d08f8699d350b972f40aa19205b6b
ise-208 <seed.0ri00@gmail.com>
2023-08-04 23:57:08 +0900

    first commit

---
+++ src/main/java/com/example/springloggersample/SpringLoggerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springloggersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoggerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoggerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springloggersample/SpringLoggerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springloggersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoggerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
